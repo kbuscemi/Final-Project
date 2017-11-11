a0596f02145958521f5c3372a0a5477e42e0c3d1
Brett Buscemi <Buscemi56@Bretts-MacBook-Air-2.local>
2017-11-10 17:22:42 -0800

    final project

---
+++ js/script.js
@@ -0,0 +1,149 @@
+$(document).ready(function(){
+
+// $(".read-more").click(slideDown);
+// function slideDown(){
+// 	event.preventDefault();
+// 	$(this).prev().show(); //show prev p
+// 	$(this).hide(); // hide read more button
+// 	$(this).next().show(); //show next which is read less
+// 	$(this).prev().next().hide;
+// }
+
+
+// $(".read-less").click(slideUp);
+// function slideUp(){
+// 	event.preventDefault();
+// 	$(this).prev().show();
+// 	$(this).hide();
+// 	$(this).next().show();
+
+// }
+
+$("#read-more-btn1").click(slideDown1);
+
+ function slideDown1(){
+ 	event.preventDefault();
+  $("#read-text-1").slideDown();
+  $(".read-less").show();
+  $(".read-more").hide();
+ }
+
+$("#read-less-btn1").click(slideUp1);
+
+ function slideUp1(){
+ 	event.preventDefault();
+   $("#read-text-1").slideUp();
+   $(".read-less").hide();
+   $(".read-more").show();
+
+ }
+ $("#read-more-btn2").click(slideDown2);
+
+ function slideDown2(){
+ 	event.preventDefault();
+  $("#read-text-2").slideDown();
+  $(".read-less").show();
+  $(".read-more").hide();
+ }
+
+$("#read-less-btn2").click(slideUp2);
+
+ function slideUp2(){
+ 	event.preventDefault();
+   $("#read-text-2").slideUp();
+   $(".read-less").hide();
+   $(".read-more").show();
+
+ }
+ $("#read-more-btn3").click(slideDown3);
+
+ function slideDown3(){
+ 	event.preventDefault();
+  $("#read-text-3").slideDown();
+  $(".read-less").show();
+  $(".read-more").hide();
+ }
+
+$("#read-less-btn3").click(slideUp3);
+
+ function slideUp3(){
+ 	event.preventDefault();
+   $("#read-text-3").slideUp();
+   $(".read-less").hide();
+   $(".read-more").show();
+
+ }
+ $("#read-more-btn4").click(slideDown4);
+
+ function slideDown4(){
+ 	event.preventDefault();
+  $("#read-text-4").slideDown();
+  $(".read-less").show();
+  $(".read-more").hide();
+ }
+
+$("#read-less-btn4").click(slideUp4);
+
+ function slideUp4(){
+ 	event.preventDefault();
+   $("#read-text-4").slideUp();
+   $(".read-less").hide();
+   $(".read-more").show();
+
+ }
+ $("#read-more-btn5").click(slideDown5);
+
+ function slideDown5(){
+ 	event.preventDefault();
+  $("#read-text-5").slideDown();
+  $(".read-less").show();
+  $(".read-more").hide();
+ }
+
+$("#read-less-btn5").click(slideUp5);
+
+ function slideUp5(){
+ 	event.preventDefault();
+   $("#read-text-5").slideUp();
+   $(".read-less").hide();
+   $(".read-more").show();
+
+ }
+ $("#read-more-btn6").click(slideDown6);
+
+ function slideDown6(){
+ 	event.preventDefault();
+  $("#read-text-6").slideDown();
+  $(".read-less").show();
+  $(".read-more").hide();
+ }
+
+$("#read-less-btn6").click(slideUp6);
+
+ function slideUp6(){
+ 	event.preventDefault();
+   $("#read-text-6").slideUp();
+   $(".read-less").hide();
+   $(".read-more").show();
+
+ }
+ $("#read-more-btn7").click(slideDown7);
+
+ function slideDown7(){
+ 	event.preventDefault();
+  $("#read-text-7").slideDown();
+  $(".read-less").show();
+  $(".read-more").hide();
+ }
+
+$("#read-less-btn7").click(slideUp7);
+
+ function slideUp7(){
+ 	event.preventDefault();
+   $("#read-text-7").slideUp();
+   $(".read-less").hide();
+   $(".read-more").show();
+
+ }
+
+})
